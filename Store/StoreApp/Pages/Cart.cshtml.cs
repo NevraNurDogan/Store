519f01874fe46079ac1e100042f50d7f272c9080
Nevra Nur Doğan <122152162+NevraNurDogan@users.noreply.github.com>
2024-08-05 23:33:08 +0300

    Store
    Oturum yönetimi düzenlendi.

--- Store/StoreApp/Pages/Cart.cshtml.cs
+++ Store/StoreApp/Pages/Cart.cshtml.cs
@@ -9,7 +9,7 @@ namespace StoreApp.Pages
     public class CartModel : PageModel
     {
         private readonly IServiceManager _manager;
-       
+
         public Cart Cart { get; set; } // IoC
         public string ReturnUrl { get; set; } = "/";
 
@@ -23,30 +23,30 @@ namespace StoreApp.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/"; //eğer boşsa ana sayfaya götür
-            // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                                          //   Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(int productId, string returnUrl)
         {
             Product? product = _manager
                 .ProductService
-                .GetOneProduct(productId,false);
+                .GetOneProduct(productId, false);
 
-            if(product is not null)
+            if (product is not null)
             {
-                // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
-                Cart.AddItem(product,1);
-                // HttpContext.Session.SetJson<Cart>("cart",Cart);
+                //   Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                Cart.AddItem(product, 1);
+                //   HttpContext.Session.SetJson<Cart>("cart",Cart);
             }
-            return RedirectToPage(new { returnUrl = returnUrl}); // returnUrl
+            return RedirectToPage(new { returnUrl = returnUrl }); // returnUrl
         }
 
         public IActionResult OnPostRemove(int id, string returnUrl)
         {
-            // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            //    Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductId.Equals(id)).Product);
             // HttpContext.Session.SetJson<Cart>("cart",Cart);
-            return Page(); 
+            return Page();
         }
     }
 }
--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,8 +1,10 @@
+using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using Repositories.Contracts;
 using Services;
 using Services.Contracts;
+using StoreApp.Models;
 
 var builder = WebApplication.CreateBuilder(args); // web uygulaması inşaa edici
 
@@ -14,6 +16,13 @@ builder.Services.AddDbContext<RepositoryContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
     b => b.MigrationsAssembly("StoreApp"));
 });
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name="StoreApp.Session";
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+});
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
@@ -23,11 +32,15 @@ builder.Services.AddScoped<IServiceManager, ServiceManager>(); // IoC yapıdaki 
 builder.Services.AddScoped<IProductService, ProductManager>(); // Configrasyon adımı
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
 
+builder.Services.AddScoped<Cart>(c=> SessionCart.GetCart(c));
+
 builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseSession ();
+
 app.UseHttpsRedirection(); // metodu, HTTP isteklerini HTTPS'ye yönlendirmek için kullanılır. 
 app.UseRouting(); // Routing middleware'ini etkinleştirir ve gelen istekleri doğru controller ve action method'larına yönlendirir.
 
